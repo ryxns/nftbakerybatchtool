7f0518b0ad3e6710916170254cffaa07349efdce
ryxns <33070164+ryxns@users.noreply.github.com>
2022-07-09 20:55:06 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
           <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-500 via-blue-300 to-red-400">
             NFTBakery Batch Buy Tool
           </h1>
-          <center><p style="color:white; font-size: 20px;">Enter the number of Sneak Peak Raffle Tickets you would like to purchase, and click the Batch Buy Button!<p></center>  
+          <center><p style="color:white; font-size: 20px;">Enter the number of Sneak Peak Raffle Tickets you would like to purchase, and click the Batch Buy Button!</p></center>  
         </div>
         <div className="mt-20 flex justify-center">
           <ChangeQuantity />
